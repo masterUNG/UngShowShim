102ec551dfe3c9c752a987b1f705ce5716e8e23e
masterUNG <phrombutr@gmail.com>
2019-10-02 12:51:43 +0700

    Design UI home Success

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,20 +9,88 @@ class _HomeState extends State<Home> {
   // Explicit
 
   // Method
+
+  Widget showLogo() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
   Widget showAppName() {
-    return Text('Ung Show Shim');
+    return Text(
+      'Ung Show Shim',
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.brown.shade700,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        fontFamily: 'Mansalva',
+      ),
+    );
+  }
+
+  Widget singInButton() {
+    return RaisedButton(
+      color: Colors.brown.shade700,
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        singInButton(),
+        SizedBox(
+          width: 4.0,
+        ),
+        signUpButton(),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              showAppName(),
-              showAppName(),
-            ],
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/wallpaper.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Container(padding: EdgeInsets.all(30.0),
+              color: Color.fromRGBO(255, 255, 255, 0.6),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  showLogo(),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  showAppName(),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  showButton(),
+                ],
+              ),
+            ),
           ),
         ),
       ),
