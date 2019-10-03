6ee8f970c0c0e6a5deec923a66d1478bfb130f07
masterUNG <phrombutr@gmail.com>
2019-10-03 12:20:55 +0700

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
 
   Widget showAppName() {
     return Text(
-      'Ung Show Shim',
+      'Ung Show Shim 123',
       style: TextStyle(
         fontSize: 30.0,
         color: MyStyle().textColor,
